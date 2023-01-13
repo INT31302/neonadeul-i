ce81823a638bbc838772236e5f6022b915ea67ae
INT31302 <tkdwo287@gmail.com>
2023-01-14 00:08:48 +0900

    refactor: 불필요한 파일 삭제

--- src/modules/holiday/dto/update-holiday.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHolidayDto } from './create-holiday.dto';
-
-export class UpdateHolidayDto extends PartialType(CreateHolidayDto) {}
