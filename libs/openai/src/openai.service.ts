57244f4377007f8b7af84f436b03845452795741
jeerryy <sangjae.kim@solarconnect.kr>
2024-07-01 16:43:33 +0900

    chore: gpt-4o으로 모델 변경

--- libs/openai/src/openai.service.ts
+++ libs/openai/src/openai.service.ts
@@ -1,17 +1,16 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { OpenaiConfig } from '@lib/openai/openai.config';
-import { Configuration, OpenAIApi } from 'openai';
+import { OpenAI } from 'openai';
 
 @Injectable()
 export class OpenaiService {
   private readonly logger = new Logger(this.constructor.name);
-  private readonly openai: OpenAIApi;
+  private readonly openai: OpenAI;
   constructor(@Inject(OpenaiConfig) private readonly configs: OpenaiConfig) {
-    const configuration = new Configuration({
-      organization: configs.organization_id,
-      apiKey: configs.secret_key,
+    this.openai = new OpenAI({
+      organization: this.configs.organization_id,
+      apiKey: this.configs.secret_key,
     });
-    this.openai = new OpenAIApi(configuration);
   }
 
   /**
@@ -20,17 +19,17 @@ export class OpenaiService {
    */
   async sendMessage(message: string): Promise<string> {
     try {
-      const response = await this.openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
+      const stream = this.openai.beta.chat.completions.stream({
+        model: 'gpt-4o',
         messages: [
           { role: 'system', content: '너는 너나들이라는 봇이야.' },
           { role: 'user', content: message },
         ],
-        max_tokens: 2000,
-        temperature: 0.7,
-        stream: false,
+        stream: true,
       });
-      return response.data.choices[0].message.content;
+
+      const chatCompletion = await stream.finalChatCompletion();
+      return chatCompletion.choices[0].message.content;
     } catch (e) {
       this.logger.error('openai 답장을 불러오는 과정 중 문제가 발생했습니다.');
       throw e;
